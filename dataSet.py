4777f132a773b869c3fc94639e60b3cd98da2416
Erick Medeiros <erick_mmedeiros@hotmail.com>
2022-02-11 21:35:38 -0300

    faturamento das distribuidoras de filmes

--- dataSet.py
+++ dataSet.py
@@ -28,14 +28,46 @@ print("*************************************************************************
 
 anos = datasetTicket["YEAR"]
 apurado = datasetTicket["TOTAL BOX OFFICE"]
+
+porcentagem = datasetDistri["MARKET SHARE"]
+nomes = datasetDistri["DISTRIBUTORS"]
+
+porcentagemFloat = []
+for x in porcentagem:
+    x = x.replace("%", "")
+    porcentagemFloat.append(float(x))
+
 apuradonovo = []
 for x in apurado:
     x = x.replace(",", "")
     x = x.replace("$", "")
     apuradonovo.append(int(x, base=10))
 
+profit = []
+for x in porcentagemFloat:
+    profit.append(round(apuradonovo[0] * (x / 100)))
+
+# data frame do registro dos lucros da indústria cinematográfica
+
+
 df = pd.DataFrame(
     {"anos": anos, "apurado": apuradonovo},
 )
-ax = df.plot.bar(x="anos", y="apurado", title="Crecimento de vendas de senhas", rot=0)
+ax = df.plot.bar(x="anos", y="apurado", title="Registro dos lucros 1995-2021", rot=0)
+
+# data frame da distribuição percentual dos lucros por distribuidora de filmes (top 10)
+
+dfPie = pd.DataFrame(
+    {"distribuidoras": profit, "radius": profit},
+    index=nomes,
+)
+plot = dfPie.plot.pie(y="distribuidoras")
+
+# data frame do cálculo do faturamento individual no ano de 2020
+
+dfBar = pd.DataFrame(
+    {"nomes": nomes, "profit": profit},
+)
+fx = dfBar.plot.area(x="nomes", title="Valor por distrubuidora", stacked=False)
+
 plt.show()
